27e8815afb633aaf9a99a956254b99f914a1fcef
Christopher Bohlman <christophermbohlman@gmail.com>
2024-12-07 02:27:40 -0500

    day 7 part2

--- 7/7.py
+++ 7/7.py
@@ -1,32 +1,42 @@
-def solve(target, currs, remaining):
-    # print(target)
+def solve1(target, currs, remaining):
     while True:
-        # print(f'Currs: {currs}')
-        # print(f'Remaining: {remaining}')
         out = []
         if len(remaining) == 0:
             for curr in currs:
                 if curr == target:
                     return True
             return False
-        # print(currs)
         for curr in currs:
-            # print(f'Current: {curr}')
             add = curr + remaining[0]
-            # print(f'Add: {add}')
-            # add = 3
             mul = curr * remaining[0]
-            # print(f'mul: {mul}')
-            # mul = 2
             if add <= target:
                 out.append(add)
             if mul <= target:
                 out.append(mul)
-            # print(f'iterout: {out}')
-        # print(out)
         remaining = remaining[1:]
         currs = out
-        #remaining = [3]
+
+def solve2(target, currs, remaining):
+    while True:
+        out = []
+        if len(remaining) == 0:
+            for curr in currs:
+                if curr == target:
+                    return True
+            return False
+        for curr in currs:
+            add = curr + remaining[0]
+            mul = curr * remaining[0]
+            op = int(str(curr) + str(remaining[0]))
+            if add <= target:
+                out.append(add)
+            if mul <= target:
+                out.append(mul)
+            if op <= target:
+                out.append(op)
+        remaining = remaining[1:]
+        currs = out
+
 
 
 def part1(data):
@@ -36,12 +46,23 @@ def part1(data):
     for line in data:
         val = int(line.split(':')[0])
         nums = list(map(int, line.split(':')[1].strip().split()))
-        print(f'On {progress} of {total}')
+        # print(f'On {progress} of {total}')
         progress += 1
-        # print('--------')
-        # print(val, nums)
-        valid = solve(val, [nums[0]], nums[1:])
-        print(valid)
+        valid = solve1(val, [nums[0]], nums[1:])
+        if valid:
+            count += val
+    return count
+
+def part2(data):
+    count = 0
+    progress = 1
+    total = len(data)
+    for line in data:
+        val = int(line.split(':')[0])
+        nums = list(map(int, line.split(':')[1].strip().split()))
+        # print(f'On {progress} of {total}')
+        progress += 1
+        valid = solve2(val, [nums[0]], nums[1:])
         if valid:
             count += val
     return count
@@ -49,4 +70,7 @@ def part1(data):
 
 with open('7.in') as f:
     data = f.read().split('\n')
+    print('Part 1: ')
     print(part1(data))
+    print('Part 2: ')
+    print(part2(data))
