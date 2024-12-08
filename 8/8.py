93753147ad89642350deb3c4dab1b0fe1153b22c
Christopher Bohlman <christophermbohlman@gmail.com>
2024-12-08 18:21:15 -0500

    part 2 complete code is still very rough

--- 8/8.py
+++ 8/8.py
@@ -17,6 +17,7 @@ class Point:
             return False
         return True
 
+
 def get_antinodes1(first, second):
     if not first and second:
         return None
@@ -55,46 +56,48 @@ def get_antinodes1(first, second):
     res.append(a2)
     return res
 
-def get_antiondes_in_line(p1, y_max, x_max):
-    
+def get_antinodes_in_line(p1, x_diff, y_diff, y_max, x_max):
+    res = []
+    res.append(p1)
+    count = 0
+    p2 = p1
+    while True:
+        a1 = Point(p1.y + y_diff, p1.x + x_diff)
+        # print(f'p1: {a1}')
+        a2 = Point(p2.y - y_diff, p2.x - x_diff)
+        # print(f'p2: {a2}')
+        # count += 1
+        # if count > 1:
+        #     break
+        if a1.in_range(y_max, x_max):
+            res.append(a1)
+        if a2.in_range(y_max, x_max):
+            res.append(a2)
+        if not(a1.in_range(y_max, x_max)) and not(a2.in_range(y_max, x_max)):
+            # print('breaking')
+            break
+        p1 = a1
+        p2 = a2
+    return res
 
 def get_antinodes2(first, second, y_max, x_max):
     if not first and second:
         return None
     y1, x1 = first
+    # print(y1, x1)
     y2, x2 = second
-    y_diff = abs(y2-y1)
-    x_diff = abs(x2-x1)
-    print(y_diff)
-    print(x_diff)
-    x_dir = y_dir  = None
+    # print(y2, x2)
+    y_diff = y2-y1
+    x_diff = x2-x1
+    y_diff_abs = abs(y_diff)
+    x_diff_abs = abs(x_diff)
+    # print(f'y_diff {y_diff}')
+    # print(f'x_diff {x_diff}')
 
-    a1 = Point(None, None)
+    a1 = Point(y1, x1)
     a2 = Point(None, None)
-    if x2 > x1:
-        
-        a1.x = x1 - x_diff
-        a2.x = x2 + x_diff
-    elif x2 < x1:
-        a1.x = x1 + x_diff
-        a2.x = x2 - x_diff
-    else:
-        a1.x = x1
-        a2.x = x2
-    
-    if y2 > y1: 
-        a1.y = y1 - y_diff
-        a2.y = y2 + y_diff
-    elif y2 < y1:
-        a1.y = y1 + y_diff
-        a2.y = y2 - y_diff
-    else:
-        a1.y = y1
-        a2.y = y2
-
-    res = []
-    res.append(a1)
-    res.append(a2)
+    # print(f'a1 {a1}')
+    res = get_antinodes_in_line(a1,x_diff, y_diff, y_max, x_max)
     return res
 
 def part1(grid):
@@ -128,8 +131,8 @@ def part2(grid):
     ants = defaultdict(set)
     max_y = len(grid)
     max_x = len(grid[0])
-    get_antinodes2((5,5),(4,3), max_y, max_x)
-    return
+    # get_antinodes2((3,4),(5,5), max_y, max_x)
+    # return
     for y in range(max_y):
         for x in range(max_x):
             val = grid[y][x]
@@ -144,7 +147,7 @@ def part2(grid):
         for i in range(len(locs)):
             # print(f'first: {locs[i]}')
             for j in range(i + 1, len(locs)):
-                res = get_antinodes1(locs[i], locs[j])
+                res = get_antinodes2(locs[i], locs[j], max_y, max_x)
                 for point in res:
                     if point.in_range(max_y, max_x):
                         antinodes.append(point.to_tuple())
@@ -153,7 +156,7 @@ def part2(grid):
           
 
 
-with open('8.test') as f:
+with open('8.in') as f:
      grid = [list(x) for x in f.read().split('\n')]
      print('Part 1: ')
      print(part1(grid))
